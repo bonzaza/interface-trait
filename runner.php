6a44bdc2f18557aee3cc5f281aa5d3e1fb74e7aa
bonzaza <bonzaza@yandex.com>
2024-02-18 12:10:55 +0100

    Refactor runner rename child variable

--- runner.php
+++ runner.php
@@ -6,5 +6,5 @@ require_once __DIR__ . '/Behavior.php';
 require_once __DIR__ . '/Implementation.php';
 require_once __DIR__ . '/CoolTrait.php';
 
-$child = new Implementation();
-$child->save('test ' . random_int(0, 9));
+$impl = new Implementation();
+$impl->save('test ' . random_int(0, 9));
